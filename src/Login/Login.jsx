c80905f7c1a5725b28f2f96b4e87eea52428a9e9
Sachin Gupta <9136167692@users.noreply.github.com>
2024-04-19 00:22:03 +0530

    update

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
         <div>
             <section class="">
                 <div class="container">
-                    <div class="row signup-form-bg mb-5">
+                    <div class="row signup-form-bg mb-5 mt-5">
                         <div class="col-sm-12 col-md-6 col-lg-6 rounded d-flex align-items-center justify-content-center">
                             <div class="login-card py-3">
                                 <h2 class="text-center font-dark pb-3">Welcome</h2>
